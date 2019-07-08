73a960f9dedda43813e99d1d030e70722bee8d93
dan <dantheman.py@gmail.com>
2019-07-08 01:09:46 +0100

    Added bit shifting macros

--- source/base.h
+++ source/base.h
@@ -7,6 +7,11 @@
 #define NULL (0UL)
 #define __REG volatile
 
+#define __SET(reg, bit)     ((reg) |= (bit))
+#define __CLEAR(reg, bit)   ((reg) &= ~(bit))
+#define __READ(reg, bit)    ((reg) & (bit))
+#define __MODIFY(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
 
 
 #endif
