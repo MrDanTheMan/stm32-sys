0fc8556d1fad0900be952ec897efad76b53cc0fe
dan <dantheman.py@gmail.com>
2019-07-08 01:10:35 +0100

    Extended STMF401RE platform macros

--- source/platform.h
+++ source/platform.h
@@ -22,6 +22,15 @@
 #define MAX_CLK_FREQ (84000000UL)
 #define MAX_AHB1_CLK_FREQ (42000000UL)
 #define MAX_AHB2_CLK_FREQ (84000000UL)
+#define MAX_FLASH_CLK_FREQ (24000000UL)
+
+// PLL Coeficients
+// NOTE: These should be calculated at runtime depending on platform clock specs
+// Output clock formula ((InputFreq / PLL_M) * PLL_N) / PLL_P
+#define PLL_M  MAX_HSI_CLK_FREQ / 1000000UL
+#define PLL_N  336U
+#define PLL_P  1U //(DIV 4)
+#define PLL_Q  8U
 
 #endif
 
